e19314ea6f1765cab2ab2e09c232d1bac9c8f231
Taylor Bleizeffer <taylorbleizeffer@gmail.com>
2023-10-03 15:23:20 -0600

    Update ProjectCarousel.js
    Remove unfinished/broken project links.

--- src/components/ProjectCarousel.js
+++ src/components/ProjectCarousel.js
@@ -28,12 +28,6 @@ const ProjectCarousel = (props) => (
            <ProjectCard title="Codewars Profile" href="https://www.codewars.com/users/T-Factorial" img={codewars_thumb} />
        </Carousel.Item>
        <Carousel.Item>
-           <ProjectCard title="Garden Tracker Android App (W.I.P)" href="https://github.com/tbzeff/GardenTracker" img={garden_thumb} />
-       </Carousel.Item>
-       <Carousel.Item>
-           <ProjectCard title="The Boop Board (works best in Firefox)" href="https://theboopboard.com" img={boop_thumb} />
-       </Carousel.Item>
-       <Carousel.Item>
            <ProjectCard title="Fathom All the Animals debut EP - To Kill a Songbird" href="https://open.spotify.com/album/078ew3gL8BLLpPopEx360I" img={fata_thumb} />
        </Carousel.Item>
     </Carousel>
