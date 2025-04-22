c90db761db0e743415894d0538d200389bf73a63
Taylor Bleizeffer <taylorbleizeffer@gmail.com>
2025-04-21 22:56:57 -0600

    Changes to page header and profile picture

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -1,24 +1,30 @@
 import React from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import selfie from '../components/res/profile_pic4.JPG';
 import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 const PageHeader = () => (    
-    <Container className="m-auto d-flex justify-content-evenly mb-3 csecondary border card-custom">
+    <Container className="m-auto d-flex justify-content-evenly mb-3 mt-3 csecondary border card-custom">
 
         <Row>
             <Col>
                 <Card className="m-auto p-auto csecondary border-0">
-                    <Card.Body className="m-auto">
+                    <Card.Body className="m-auto mt-3">
                         <Card.Title className="text-center ctext" variant="center"><strong>Taylor Bleizeffer</strong></Card.Title> 
                         <Card.Text className="fw-bold ctext">
                         A Computer Science graduate from the University of Wyoming who is deeply curious about the inner-workings of the world. With an academic background in computation and research, and personal background in music and entertainment, I'm always ready to approach problems with an open mind.
                         </Card.Text>
                     </Card.Body>
                 </Card>    
+            </Col>
+            <Col>
+                <Container className="d-flex justify-content-evenly mt-3 mb-3 card-custom">
+                    <img className="img-fluid" src={selfie} alt="Portrait of Taylor Bleizeffer"/>
+                </Container>
             </Col>
         </Row>
                
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Col, Row } from 'react-bootstrap';
-import selfie from '../components/res/profile_pic4.JPG';
 import PageHeader from '../components/PageHeader';
 import ContactLinks from '../components/ContactLinks';
 import ProjectCarousel from '../components/ProjectCarousel';
@@ -10,14 +9,8 @@ import ProjectCarousel from '../components/ProjectCarousel';
 const Home = () => (
     <Container className="cprimary" fluid>
         <Col className="m-auto col-md-6">
-            
-            <Row>
-                <Container className="d-flex justify-content-evenly mt-3 card-custom">
-                    <img className="img-fluid" src={selfie} alt="Portrait of Taylor Bleizeffer"/>
-                </Container>
-            </Row>
-            <Row><ContactLinks /></Row>
             <Row><PageHeader /></Row>
+            <Row><ContactLinks /></Row>
             <Row><ProjectCarousel /></Row>
         </Col>
     </Container>
